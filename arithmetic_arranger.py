f662184f2a33aa5446f9425f5546075081b10d49
sung pham <s.phamv@hotmail.com>
2020-11-02 00:01:23 +0100

    Working function

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,25 +1,16 @@
-# function takes 2 arguments -> list of strings + boolean flag
-
-
-
-
-
-
-# single space between operator and longest of the operands
-# operator one same line as second operand
-# four spaces between each problem
-# dashes at the bottom of each problem
-
-# pull out the list and split the strings
-# check 
-# 
-
-
-def arithmetic_arranger(problems, solution):
-    bol = solution
+def arithmetic_arranger(*args):
+    problems=args[0]
+    # check if input of problems has the correct format
+    if len(problems[0].split()) != 3:
+        return "Error: Please enter a problem in the form 'x + y'"
 
     # max 5 entries in list
-    if len(arr) > 5: return "Error: Too many problems"
+    if len(problems) > 5: return "Error: Too many problems."
+
+    operator1 = []
+    operator2 = []
+    operand = []
+    solutions = []
 
     for problem in problems:
         no1 = problem.split()[0]
@@ -27,10 +18,10 @@ def arithmetic_arranger(problems, solution):
         op = problem.split()[1]
 
         # operator only '+' or '-'
-        if op not in ['+','-']: return 'Error: Operator must be '+' or '-''
+        if op not in ['+','-']: return "Error: Operator must be '+' or '-'."
 
         # max 4 digit numbers
-        if len(no1)>4 | len(no2)>4: return "Error: Numbers cannot be more than four digits"
+        if len(no1) > 4 or len(no2)>4: return "Error: Numbers cannot be more than four digits."
         
         # numbers only digits
         try: 
@@ -39,6 +30,50 @@ def arithmetic_arranger(problems, solution):
         except:
             return 'Error: Numbers must only contain digits.'
 
+        operator1.append(no1)
+        operator2.append(no2)
+        operand.append(op)
+        
+        #calculate solutions and put into solutions list
+    for i in range (0,len(problems)):
+        if(operand[i] == "+"):
+            solutions.append(operator1[i] + operator2[i])
+        else: 
+            solutions.append(operator1[i] - operator2[i])
+        
+    line1 = ""
+    line2 = ""
+    line3 = ""
+    line4 = ""
 
+    for i in range(0, len(problems)):
+        if operator1[i] > operator2[i]:
+            line1 += (" " * 2) + str(operator1[i]) + (" " * 4)
+            line2 += operand[i] + " " + " " * (len(str(operator1[i])) - len(str(operator2[i]))) + str(operator2[i]) + (" " * 4)
+            line3 += "-" * (len(str(operator1[i]))+2) + (" " * 4)
 
-    return arranged_problems
+            if len(str(solutions[i])) > len(str(operator1[i])):
+                line4 += " " * (len(str(solutions[i])) - len(str(operator1[i]))) + str(solutions[i]) + (" " * 4)
+            elif len(str(solutions[i])) < len(str(operator1[i])):
+                line4 += " " * (len(str(operator1[i])) - len(str(solutions[i]))) + str(solutions[i]) + (" " * 4)
+            else:
+                line4 += " " * 2 + str(solutions[i]) + (" " * 4)
+
+        else:
+            line1 += " " * 2 + " "*(len(str(operator2[i])) - len(str(operator1[i]))) + str(operator1[i]) + (" " * 4)
+            line2 += operand[i] + " " + str(operator2[i]) + (" " * 4)
+            line3 += "-" * (len(str(operator2[i]))+2) + (" " * 4)
+
+            if len(str(solutions[i])) > len(str(operator2[i])):
+                line4 += " " * (len(str(solutions[i])) - len(str(operator2[i]))) + str(solutions[i]) + (" " * 4)
+            elif len(str(solutions[i])) < len(str(operator2[i])):
+                line4 += " " * (len(str(operator2[i])) - len(str(solutions[i]))) + str(solutions[i]) + (" " * 4)
+            else:
+                line4 += " " * 2 + str(solutions[i]) + (" " * 4)
+
+    try:
+        if args[1]==True:
+            return line1[:len(line1)-4] + '\n' + line2[:len(line2)-4] + '\n' + line3[:len(line3)-4] + '\n' + line4[:len(line4)-4]
+    except:
+        return line1[:len(line1)-4] + '\n' + line2[:len(line2)-4] + '\n' + line3[:len(line3)-4]
+
